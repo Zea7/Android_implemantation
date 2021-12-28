a8b6827d5c367a215620531d7f13f6966f64080c
Yunjeong Choi <yoonjeong04@sookmyung.ac.kr>
2021-12-28 22:28:37 +0900

    중간저장

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,5 +1,36 @@
 package com.example.test;
 
-public class MainActivity {
-    asdfasdf
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.Objects;
+
+
+public class MainActivity extends AppCompatActivity {
+    private final int[] tabIcons = {
+      R.drawable.contact,
+      R.drawable.gallery,
+      R.drawable.note
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager vp = findViewById(R.id.viewpager);
+        VPAdapter adapter = new VPAdapter(getSupportFragmentManager());
+        vp.setAdapter(adapter);
+
+
+        // 연동
+        TabLayout tab = findViewById(R.id.tab);
+        tab.setupWithViewPager(vp);
+
+        for(int i=0;i<3;i++) Objects.requireNonNull(tab.getTabAt(i)).setIcon(tabIcons[i]);
+
+    }
 }
