4bceb10ce45b78906fe3586082021f73007afe61
Zea <zeaear714@gmail.com>
2021-12-28 19:58:17 +0900

    Make TabLayout

---
+++ app/src/main/java/com/example/test/VPAdapter.java
@@ -0,0 +1,31 @@
+package com.example.test;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class VPAdapter extends FragmentPagerAdapter{
+    private ArrayList<Fragment> items;
+
+    public VPAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+        items = new ArrayList<Fragment>();
+        items.add(new Fragment1());
+        items.add(new Fragment2());
+        items.add(new Fragment3());
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+}
