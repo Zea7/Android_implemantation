3b2f63fd60867a6cd04e06d24b5ea7b2aeb7cfd2
Yunjeong Choi <yoonjeong04@sookmyung.ac.kr>
2021-12-29 12:22:13 +0900

    Person 클래스 생성

--- contact/src/main/java/com/example/contact/ContactActivity.java
+++ contact/src/main/java/com/example/contact/ContactActivity.java
@@ -1,11 +1,10 @@
 package com.example.contact;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class ContactActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class ContactActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ contact/src/main/java/com/example/contact/Person.java
@@ -0,0 +1,27 @@
+package com.example.contact;
+
+public class Person {
+    String Name;
+    String PhoneNumber;
+
+    public Person(String name, String phoneNumber) {
+        Name = name;
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+}
