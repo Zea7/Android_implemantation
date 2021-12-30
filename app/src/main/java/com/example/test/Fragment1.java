e87bf2edf1b2e580f380d53ec285a93969691f87
Yunjeong Choi <yoonjeong04@sookmyung.ac.kr>
2021-12-30 18:04:30 +0900

    휴대폰 안의 연락처 정렬된 형태로 가져오기 완료

--- app/src/main/java/com/example/test/Fragment1.java
+++ app/src/main/java/com/example/test/Fragment1.java
@@ -1,5 +1,7 @@
 package com.example.test;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
 import android.graphics.Rect;
 import android.os.Bundle;
 
@@ -7,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,7 +34,6 @@ public class Fragment1 extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private RecyclerView recyclerView;
-    private UserListAdapter adapter;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -63,43 +65,28 @@ public class Fragment1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_gallery, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_contact, container, false);
 
-        recyclerView = (RecyclerView) rootView.findViewById(R.id.icon_gallery);
-
-        List<Person> contactList = new ArrayList<Person>();
-        Person person = new Person("성민정", "010-4399-2755");
-        contactList.add(person);
-
-        person = new Person("복승주",  "010-6679-3956");
-        contactList.add(person);
-
-        person = new Person("탁유성",  "010-7810-3987");
-        contactList.add(person);
-
-        person = new Person("예효기",  "010-6936-3816");
-        contactList.add(person);
-
-        person = new Person("박병욱",  "010-4010-8889");
-        contactList.add(person);
-
-        person = new Person("홍주연",  "010-4043-2603");
-        contactList.add(person);
-
-        person = new Person("강원재",  "010-5152-5826");
-        contactList.add(person);
-
-        person = new Person("송지현",  "010-2345-8474");
-        contactList.add(person);
-
-        person = new Person("안원준",  "010-2618-3674");
-        contactList.add(person);
-        adapter = new UserListAdapter(contactList);
+        recyclerView = rootView.findViewById(R.id.contact);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        RecyclerView.LayoutManager layoutManager = linearLayoutManager;
+        recyclerView.setLayoutManager(layoutManager);
+        ContactsRvAdapter adapter = new ContactsRvAdapter(getContext(), getContacts());
         recyclerView.setAdapter(adapter);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(layoutManager);
-
         return rootView;
+    }
+
+
+    private List<ModelContacts> getContacts(){
+        List <ModelContacts> list = new ArrayList<>();
+        Cursor cursor = getContext().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null, null, null, ContactsContract.Contacts.DISPLAY_NAME + " ASC");
+        cursor.moveToFirst();
+        while (cursor.moveToNext()){
+            list.add(new ModelContacts(cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER))));
+        }
+        return list;
     }
 }
---
+++ contact/src/main/java/com/example/contact/ModelContacts.java
@@ -0,0 +1,26 @@
+package com.example.contact;
+
+public class ModelContacts {
+    private String name, phoneNumber;
+
+    public ModelContacts(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
